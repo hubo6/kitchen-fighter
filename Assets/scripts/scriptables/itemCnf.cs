88d00476e9c9ca3b38c8871b425249c9048938d3
cooper <cooper@bluemyth.ca>
2023-04-26 20:30:55 -0700

    a)updated the class naming p1

---
+++ Assets/scripts/scriptables/itemCnf.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class itemCnf : ScriptableObject
+{
+    // Start is called before the first frame update
+    public Transform prefab;
+    public ITEM_MSK msk;
+    public ITEM_TYPE type;
+    public Sprite icon;
+    public float height;
+    public int stat = 0;
+}
+
+
+
+public enum ITEM_TYPE { 
+PLATE = 0,
+RAW,
+PROCESSED,
+BURNT,
+}
+public enum ITEM_MSK { 
+    NONE = 0,
+    TOMATO,
+    CHEESE,
+    MEAT_PIE,
+}
